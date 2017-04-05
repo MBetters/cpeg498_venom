0aa5a25c53af4fdc1b45aceb6b387a646771e9a2
Danww93 <danww@udel.edu>
2017-04-05 13:56:15 -0400

    Cleaned up force sensor code

--- spider_arduino/examples/force_sensor_example.ino
+++ spider_arduino/examples/force_sensor_example.ino
@@ -2,23 +2,17 @@
  // Current pins being used by servos: 3, 4, 5, 6, 9, 10, 20, 21, 22, 23, 25, 32
  // Possible pins for each force sensor: 14, 15, 16, 17, 18, 19
 int fsrFLL = 14; // FSR for Front Left Leg
-//int fsrReadingFLL;
 int fsrMLL = 15; // Middle Left Leg
-//int ReadingMLL; 
 int fsrBLL = 16; // Back Left Leg
-//int fsrReadingBLL; 
 int fsrFRL = 17; // Front Right Leg
-//int fsrReadingFRL;
 int fsrMRL = 18; // Middle Right Leg
-//int fsrReadingMRL;
 int fsrBRL = 19; // Back Right Leg
-//int fsrReadingBRL;
  
-void setup(void) {
+void setup() {
   Serial.begin(9600);   // Send debugging information via the Serial monitor
 }
  
-void loop(void) {
+void loop() {
   getSensorRead(fsrFLL); // Gets sensor reading for FLL and outputs its respective value.
   getSensorRead(fsrMRL);
   getSensorRead(fsrBRL);
