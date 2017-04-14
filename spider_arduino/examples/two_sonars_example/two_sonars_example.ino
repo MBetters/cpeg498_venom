b93fb8111472f6804d40afcb0ba7a991f4011ee0
Danww93 <danww@udel.edu>
2017-04-14 14:18:42 -0400

    updated sonar code

--- spider_arduino/examples/two_sonars_example/two_sonars_example.ino
+++ spider_arduino/examples/two_sonars_example/two_sonars_example.ino
@@ -8,26 +8,13 @@ int echoPin2 =8;
 int initPin2 =7;
 int distance2 =0;
 
-/*//Sonar 3
-int echoPin3 =8;
-int initPin3 =9;
-int distance3 =0;
-
-//Sonar 4
-int echoPin4 =12;
-int initPin4 =13;
-int distance4 =0;*/
-
 void setup() {
   
   pinMode(initPin1, OUTPUT);
   pinMode(echoPin1, INPUT);
   pinMode(initPin2, OUTPUT);
   pinMode(echoPin2, INPUT);
-  /*pinMode(initPin3, OUTPUT);
-  pinMode(echoPin3, INPUT);
-  pinMode(initPin4, OUTPUT);
-  pinMode(echoPin4, INPUT);*/
+
   Serial.begin(9600);
   
 }
@@ -43,13 +30,7 @@ void loop() {
   delay(150);
 
   Serial.println(" ");
-  
-  /*distance3 = getDistance(initPin3, echoPin3);
-  printDistance(3, distance3);
-  delay(150);
-  
-  distance4 = getDistance(initPin4, echoPin4);
-  printDistance(4, distance4);*/
+ 
 
   delay(500);
   
